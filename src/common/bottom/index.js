d662701ec93f353a6fa18de2e8eec7f92be27db5
joy_li <woaiyinle520@163.com>
2020-08-18 23:09:20 +0800

    简书项目提交

---
+++ src/common/bottom/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { BottomBox } from './style'
+
+export default class Bottom extends Component {
+  render() {
+    return (
+      <div>
+        <BottomBox>底部</BottomBox>
+      </div>
+    )
+  }
+}
---
+++ src/common/bottom/style.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const BottomBox = styled.div`
+  z-index: 2;
+  height: 100px;
+  width: 100%;
+  background-color: red;
+  text-align:center;
+  line-height:100px
+`
